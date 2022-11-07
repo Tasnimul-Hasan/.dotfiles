51ccf37cfef9027edaf0dce4020561a00e3b1121
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-11-07 22:08:33 +0600

    Update node.sh

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,14 +1,16 @@
 if [ `nvm current` == system ]; then
-  echo 'Installing node...'
+  echo 'Installing latest version of NodeJs...'
   rm ~/.nodejs /opt/nodejs -rf
   nvm install --lts
   nvm use --lts
   clear
   
-  echo "Installing global npm packages..."
+  echo 'Rebuilding ~/.bashrc'
+  cat ~/scripts/main.sh > ~/.bashrc
+  clear
+  
+  echo 'Installing global npm packages...'
   npm i -g npm@latest
   npm i -g nodemon live-server gitignore
   clear
-  
-  cat ~/scripts/main.sh > ~/.bashrc
 fi
